8449463b283c8b459a13a2f037802f8cc9867c9a
Danilo Yorinori <daniloksy@gmail.com>
2020-10-12 19:47:15 -0300

    Utilização de mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
 
 import '../styles/pages/orphanages-map.css';
 import mapMarkerImg from '../assets/images/map-marker.svg';
 
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
   return (
@@ -23,7 +25,14 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <Map
+        center={[-25.4263987,-49.2728055]}
+        zoom={13.75}
+        style={{ width: '100%', height: '100%'}} 
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={36} color="#FFF" />
